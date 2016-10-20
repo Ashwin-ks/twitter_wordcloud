29709105b8f752d91382859ebe4f6af0075775d9
Ashwin K <ashwinkanderi@gmail.com>
2016-10-20 15:55:53 -0400

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,11 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App
+
+
+def oauth():
+    return {"consumer_key": "tpTuHxAKUvaayVzpIPwT6MMz1",
+            "consumer_secret": "QuMPaFE2B6kLNnY1AZ3zzBcUY21dz0eQ3DdxadzhjMfKL3PGD3",
+            "token_key": "2805288875-yEfLb2rv6M7ECGlIuXfL2bU9DmIAt51x9as4Ubz",
+            "token_secret": "7GFkPjnWqMUKErvrDMnW1ORpSwG1YMzNmHbNsd8yR47p1"}
